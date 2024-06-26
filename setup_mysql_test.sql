d1c160059b447b0942c54c3480b73c17f5b01990
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-26 12:44:01 +0300

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user and set passwd
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant priviledges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant user select to performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Appply priviledges
+FLUSH PRIVILEGES;
