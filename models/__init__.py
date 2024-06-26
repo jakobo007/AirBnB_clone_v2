3f73e4c2b82a0c24c6e736af8a4ef39fd2cf311b
jakobo007 <oumajames>
2024-06-26 14:34:55 -0700

    Circular import

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,15 @@
 """This module handles the storage of HBNB objects"""
 
 from os import getenv
+from models.engine.db_storage import DBStorage
 
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 if HBNB_TYPE_STORAGE == 'db':
-    from models.engine.db_storage import DBStorage
+    #from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
+    #from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
