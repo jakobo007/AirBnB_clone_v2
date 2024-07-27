6153ba85a52da575c12f65e06f33a94a7920af7c
jakobo007 <oumajames>
2024-07-27 13:56:31 -0700

    Flask_1

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,10 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Table
-from sqlalchemy.orm import Relationship
+from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = Relationship("Place", secondary=place_amenity, back_populates='amenities')
+    place_amenities = relationship("Place", secondary=place_amenity, back_populates='amenities')
