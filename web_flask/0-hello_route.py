561a7897d403c9f2a11e62ae800293cb2d5dc5cf
jakobo007 <oumajames>
2024-07-27 13:43:52 -0700

    Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that starts 
+a flask web application"""
+from flask import flask
+app = Flask(__name__)
+
+app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBHB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
