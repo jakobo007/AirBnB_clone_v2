f0b26e7f712edda5cb58bb9ec5d0f87a90b2015d
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-07-28 19:33:16 +0300

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
     return "HBHB"
 
 
-app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """Returns given string"""
     return "C {}".format(text.replace("_", " "))
