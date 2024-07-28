a9337142f2887dc0e53d187669fe412fdb1a3e49
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-07-28 19:45:30 +0300

    Python is cool_2

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,26 +9,26 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """Display Hello HBNB"""
-    return "Hello HBNB!"
+    return ("Hello HBNB!")
 
 
 @app.route("/HBHB", strict_slashes=False)
 def hbnb():
     """Display HBNB"""
-    return "HBHB"
+    return ("HBHB")
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """Returns given string"""
-    return "C {}".format(text.replace("_", " "))
+    return ("C {}".format(text.replace("_", " ")))
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """Returns given string"""
-    return "Pyton {}".format(text.replace("_", " "))
+    return ("Pyton {}".format(text.replace("_", " ")))
 
 
 if __name__ == "__main__":
