1306147c9ca5026773d9b2f92d5d4c231d5e9a1c
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-07-28 19:25:06 +0300

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,7 +13,7 @@ def hello():
     return "Hello HBNB!"
 
 
-@app.route("/HBHB", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Display HBNB"""
     return "HBHB"
