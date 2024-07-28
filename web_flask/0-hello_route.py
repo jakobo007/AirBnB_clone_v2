922abbf5a6a0387c6486f312bcb1ab501e8bdd34
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-07-28 18:17:59 +0300

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,4 +7,9 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """Display Hello HBNB"""
     return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
