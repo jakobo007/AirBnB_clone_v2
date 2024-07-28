4ded7613ee7665237d5e9c7b6ef17ad980faaea7
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-07-29 02:09:14 +0300

    odd or even 2

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,7 +49,7 @@ def number_template(n):
 def is_odd_or_even(n):
     """Display page if n=int and is odd/even"""
     if isinstance(n, int):
-        return render_template("6-number_even_or_odd.html", n=n)
+        return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
