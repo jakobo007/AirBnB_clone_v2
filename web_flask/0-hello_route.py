e0bb169a81e2ab4645a001416d423f2641fb6fa6
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-07-28 19:29:48 +0300

    HBNB2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Script that starts
-a Flask web application"""
-
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
+
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Display Hello HBNB"""
-    return "Hello HBNB!"
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000, debug=None)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Script that starts
-a Flask web application"""
-
+""" Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
-@app.route("/", strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello():
-    """Display Hello HBNB"""
-    return "Hello HBNB!"
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Display HBNB"""
-    return "HBHB"
+    """Returns a given string"""
+    return ("HBNB")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000, debug=None)
