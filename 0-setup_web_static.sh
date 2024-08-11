eb648ee32035c2984d1902c387c711a9540cd41e
jakobo007 <oumajames>
2024-08-11 02:03:26 -0700

    Static setup20

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,6 +22,7 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership of /data/ to ubuntu
 sudo chown -R ubuntu:ubuntu /data/
+sudo chmod -R 755 /data/
 
 # Update Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 sudo sed -i '/server_name _;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
