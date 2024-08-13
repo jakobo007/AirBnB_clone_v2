8a642a3de84249cd61324ccd5ae078c1a4908f59
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-08-14 01:01:43 +0300

    Full Deployment2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,8 +2,17 @@
 """Imported Modules"""
 from fabric.api import env
 from os.path import exists
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+import importlib.util
+
+# Dynamically import 1-pack_web_static
+pack_spec = importlib.util.spec_from_file_location("pack_web_static", "1-pack_web_static.py")
+pack_web_static = importlib.util.module_from_spec(pack_spec)
+pack_spec.loader.exec_module(pack_web_static)
+
+# Dynamically import 2-do_deploy_web_static
+deploy_spec = importlib.util.spec_from_file_location("do_deploy_web_static", "2-do_deploy_web_static.py")
+do_deploy_web_static = importlib.util.module_from_spec(deploy_spec)
+deploy_spec.loader.exec_module(do_deploy_web_static)
 
 env.hosts = ['100.26.227.224', '34.227.92.200']
 
